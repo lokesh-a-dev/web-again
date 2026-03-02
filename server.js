1a3b7ef7bfd35d1378c8ba41b26ea0c766c637bd
lokeshRedEye <lokeshcoder123@gmail.com>
2026-03-02 13:12:06 +0530

    updete ui

--- server.js
+++ server.js
@@ -4,38 +4,50 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
 app.use(express.static("public"));
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
 
-  socket.on("join-room", roomId => {
+  socket.on("join-room", (roomId) => {
     socket.join(roomId);
+    socket.roomId = roomId;
 
-    const users = [...(io.sockets.adapter.rooms.get(roomId) || [])];
-    socket.emit("existing-users", users.filter(id => id !== socket.id));
+    const room = io.sockets.adapter.rooms.get(roomId) || new Set();
+    const users = [...room].filter(id => id !== socket.id);
 
+    socket.emit("existing-users", users);
     socket.to(roomId).emit("new-user", socket.id);
+  });
 
-    socket.on("offer", ({ offer, to }) => {
-      socket.to(to).emit("offer", { offer, from: socket.id });
-    });
+  socket.on("offer", ({ offer, to }) => {
+    socket.to(to).emit("offer", { offer, from: socket.id });
+  });
 
-    socket.on("answer", ({ answer, to }) => {
-      socket.to(to).emit("answer", { answer, from: socket.id });
-    });
+  socket.on("answer", ({ answer, to }) => {
+    socket.to(to).emit("answer", { answer, from: socket.id });
+  });
 
-    socket.on("ice-candidate", ({ candidate, to }) => {
-      socket.to(to).emit("ice-candidate", { candidate, from: socket.id });
-    });
+  socket.on("ice-candidate", ({ candidate, to }) => {
+    socket.to(to).emit("ice-candidate", { candidate, from: socket.id });
+  });
 
-    socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-left", socket.id);
-    });
-
+  socket.on("disconnect", () => {
+    if (socket.roomId) {
+      socket.to(socket.roomId).emit("user-left", socket.id);
+    }
   });
 
 });
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
